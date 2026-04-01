6f5c3dbc923533bb29d86e634de3a3a2781a9fd4
fathannnur <carelius02@gmail.com>
2026-04-01 13:18:04 +0700

    Menambahkan while

--- looping.cpp
+++ looping.cpp
@@ -49,3 +49,13 @@ bool isPrime(int num)
     if (num <= 1)
         return false;
     int i = 2;
+
+    while (i * i <= num)
+    {
+        if (num % i == 0)
+            return false;
+        i++;
+    }
+    return true;
+}
+
