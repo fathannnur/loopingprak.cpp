5555b4d8ab0cf98dca193c6bf3f116543aa804ac
fathannnur <carelius02@gmail.com>
2026-04-01 13:08:16 +0700

    Menambahkan void

--- looping.cpp
+++ looping.cpp
@@ -4,3 +4,10 @@ using namespace std;
 // variabel global
 int n;
 int pilihan;
+
+// --- Prototipe Fungsi ---
+bool isPrime(int num);
+bool isFibonacci(int num);
+void inputNumber();
+void displayResult(int type);
+void showMenu();
